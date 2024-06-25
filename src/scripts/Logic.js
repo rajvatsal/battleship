5722de900cd0064c32b154f627097fe87b90359d
Vatsal <rajvatsal2004@gmail.com>
2024-06-25 12:23:45 +0530

    feat(Logic): Add placeShip function to Gameboard

--- src/scripts/Logic.js
+++ src/scripts/Logic.js
@@ -1,12 +1,42 @@
 import { emit, on, off } from "./pub-sub.js";
 
+const shipInterface = (state) => ({
+	type: "Ship Interface",
+	isSunk: () => state.isSunk(),
+	hit: () => state.hit(),
+});
+
 export function Ship(len = 1) {
 	const MAX_LENGTH = 4;
 	const MIN_LENGTH = 1;
 	const length =
 		len > MAX_LENGTH ? MAX_LENGTH : len < MIN_LENGTH ? MIN_LENGTH : len;
 	let hitCount = 0;
-	const hit = () => ++hitCount;
-	const isSunk = () => length <= hitCount && hitCount > 0;
-	return { hit, isSunk };
+
+	const proto = {
+		hit: () => ++hitCount,
+		isSunk: () => length <= hitCount && hitCount > 0,
+	};
+
+	return Object.assign(Object.create(shipInterface(proto)));
+}
+
+export function Gameboard() {
+	const board = [];
+	for (let i = 0; i < 10; i++) {
+		board.push(Array(10).fill(null));
+	}
+
+	const getBoard = () => board;
+	const placeShip = (coord, orientation, len) => {
+		const [x, y] = coord;
+		for (let i = 0; i < len; i++) {
+			if (orientation === 90) board[i + x][y] = 1;
+			else board[x][i + y] = 1;
+		}
+
+		return Ship(len);
+	};
+
+	return { getBoard, placeShip };
 }
--- src/scripts/logic.test.js
+++ src/scripts/logic.test.js
@@ -1,13 +1,56 @@
-import { Ship } from "./Logic.js";
+import { Ship, Gameboard } from "./Logic.js";
 
 describe("Ship Factory", () => {
 	const ship = Ship(3);
 	it("Has hit()?", () => expect(ship.hit()).toBe(1));
 	it("Has hit()?", () => expect(ship.hit()).toBe(2));
 
+	it("Is part of ship interface", () =>
+		expect(Ship().type).toMatch("Ship Interface"));
+
 	it("Ship sunk false", () => expect(ship.isSunk()).toBe(false));
 	it("Ship sunk true", () => {
 		ship.hit();
 		expect(ship.isSunk()).toBe(true);
+	});
+});
+
+describe("Gameboard Factory", () => {
+	const gb = Gameboard();
+	it("Get board", () => {
+		const board = gb.getBoard();
+		expect(board.length).toBe(10);
+		for (const row of board) {
+			expect(row.length).toBe(10);
+			for (const item of row) {
+				expect(item).toBe(null);
+			}
+		}
+	});
+
+	describe("Place ship", () => {
+		const { placeShip } = gb;
+		it("Can place ship", () => {
+			placeShip([5, 5], 90, 4);
+			let shipValue = [];
+			for (let i = 5; i < 9; i++) {
+				shipValue.push(gb.getBoard()[i][5]);
+			}
+
+			expect(shipValue).toEqual([1, 1, 1, 1]);
+
+			placeShip([0, 0], 180, 2);
+			shipValue = [gb.getBoard()[0][0], gb.getBoard()[0][1]];
+
+			expect(shipValue).toEqual([1, 1]);
+		});
+
+		it("Returned Object is instance of Ship()", () => {
+			const ship = placeShip([3, 0], 90, 2);
+			expect(ship.type).toMatch(Ship().type);
+			expect(ship.hit()).toBe(1);
+			expect(ship.hit()).toBe(2);
+			expect(ship.isSunk()).toBe(true);
+		});
 	});
 });
