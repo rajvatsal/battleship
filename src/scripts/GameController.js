f5bf9752c97fb990439ab2d5be4ce3aee33733bb
Vatsal <rajvatsal2004@gmail.com>
2024-07-02 13:56:07 +0530

    refactor(GController): Rename isSunk to isShipHit

--- src/scripts/GameController.js
+++ src/scripts/GameController.js
@@ -6,8 +6,8 @@ const playerTwo = Player(1, "player-two");
 
 function _receivedAttack({ side, coords }) {
 	const attackedPlayer = playerOne.side === side ? playerOne : playerTwo;
-	const isShipSunk = attackedPlayer.receiveAttack(coords);
-	if (isShipSunk) pubsub.emit("ShowVerifiedSquares", isShipSunk);
+	const isShipHit = attackedPlayer.receiveAttack(coords);
+	if (isShipHit) pubsub.emit("ShowVerifiedSquares", isShipHit);
 	pubsub.emit("UpdateBoard", attackedPlayer.getBoard()[coords[0]][coords[1]]);
 	if (attackedPlayer.hasLost()) pubsub.emit("GameOver", attackedPlayer.side);
 }
